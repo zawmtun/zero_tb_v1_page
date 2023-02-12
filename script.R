a2a7445943134a671ba277f9c8baf0e54725f6ab
Zaw Myo Tun <zawmyotun11@gmail.com>
2023-02-12 15:56:19 +0800

    create sync_analyses()

--- script.R
+++ script.R
@@ -1,4 +1,11 @@
-file_name <- "descriptive_analysis.html"
-source_file_path <- file.path(Sys.getenv("GITHUB_PATH"), "zero_tb_v1", "doc", file_name)
-file.copy(source_file_path, file.path("analyses", file_name),
-          overwrite = TRUE)
+file_names <- c(
+  "descriptive_analysis.html"
+)
+
+sync_analyses <- function(file) {
+  source_file_path <- file.path(Sys.getenv("GITHUB_PATH"), "zero_tb_v1", "doc", file)
+  file.copy(source_file_path, file.path("analyses", file),
+            overwrite = TRUE)
+}
+
+lapply(file_names, sync_analyses)
